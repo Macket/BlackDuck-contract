bd37370cfeb68318d6549f0973ca30b9356a649a
Makeev Ivan <makeev@phystech.edu>
2021-09-04 22:57:34 +0300

    refactor: brodcastTx without console.logs

--- src/sdk/utils.ts
+++ src/sdk/utils.ts
@@ -1,12 +1,9 @@
 import { stringToBytes, sha256, base58Encode } from "@waves/ts-lib-crypto";
 import { broadcast, waitForTx } from "@waves/waves-transactions";
 
-export const broadcastTx = async (tx): Promise<string> => {
+export const broadcastTx = async (tx): Promise<void> => {
     await broadcast(tx, 'https://nodes-testnet.wavesnodes.com');
-    console.log('Transaction:', tx);
     await waitForTx(tx.id, { apiBase: 'https://nodes-testnet.wavesnodes.com' });
-    console.log('Transaction is in the blockchain: ', tx.id);
-    return tx.id
 };
 
 export type DUCKS_ORDER_TYPE = "worst,medium,best" | "worst,best,medium" | "medium,worst,best" | "medium,best,worst" | "best,worst,medium" | "best,medium,worst";

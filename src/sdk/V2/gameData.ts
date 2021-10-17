4eea0fef0beb36446ef2d95eee0fb8efebf0430f
Makeev Ivan <makeev@phystech.edu>
2021-10-17 18:49:08 +0300

    feat: sdk for V2

---
+++ src/sdk/V2/gameData.ts
@@ -0,0 +1,157 @@
+import axios from 'axios';
+import { GAME_ADDRESS, FARMING_ADDRESS, EGG_ID, WAVES_NODE } from "../../settings";
+
+
+export const getEggBalance = async (address: string): Promise<number> => {
+    const res = await axios.get(WAVES_NODE + `/assets/balance/${address}/${EGG_ID}`);
+    return res.data.balance;
+};
+
+export const getData = async (regExp): Promise<{ key: string, type: string, value: string | number | boolean }[]> => {
+    const res = await axios.get(WAVES_NODE + `/addresses/data/${GAME_ADDRESS}?matches=${regExp}`);
+    return res.data;
+};
+
+export const getBlockHeight = async (): Promise<number> => {
+    const res = await axios.get(WAVES_NODE + "/blocks/height");
+    return res.data.height;
+};
+
+export const getNextGameId = async (): Promise<number> => {
+    const gameIdData = await getData('nextGameId');
+    return gameIdData.length > 0 ? gameIdData[0].value as number : 1
+}
+
+type Duck = {assetId: string, genes: string, rarity: number};
+type Ducks = { 1: Duck[], 2: Duck[], 3: Duck[], 4: Duck[], 5: Duck[] };
+
+export const getFarmingDucks = async (address: string): Promise<Ducks> => {
+    const res = await axios.get(WAVES_NODE + `/addresses/data/${FARMING_ADDRESS}?matches=address_${address}_.*_farmingPower`);
+    const farmingDucks = res.data.filter(d => d.value > 0);
+
+    const ducks = {1: [], 2: [], 3: [], 4: [], 5: []};
+    for (let farmingDuck of farmingDucks) {
+        const assetId = farmingDuck.key.split("_")[3];
+        const genes = (await axios.get(WAVES_NODE + `/assets/details/${assetId}`)).data.name;
+        const rarity = farmingDuck.value;
+        ducks[Math.min(5, Math.ceil(rarity / 10))].push({assetId, genes, rarity})
+    }
+
+    console.log(ducks);
+
+    return ducks
+};
+
+export const getPlayerCurrentGame = async (playerAddress: string): Promise<number> => {
+    return (await getData(playerAddress + "_currentGame"))[0].value as number;
+}
+
+export const getSlot = async (slot: number): Promise<number> => {
+    return (await getData('slot' + slot.toString()))[0].value as number;
+}
+
+export const getPlayerRole = async (gameId: number, playerAddress: string): Promise<string> => {
+    return (await getData('game' + gameId.toString() + "_" + playerAddress))[0].value as string;
+}
+
+export const getMaker = async (gameId: number): Promise<string> => {
+    return (await getData('game' + gameId.toString() + '_maker'))[0].value as string;
+}
+
+export const getTaker = async (gameId: number): Promise<string> => {
+    return (await getData('game' + gameId.toString() + '_taker'))[0].value as string;
+}
+
+export const getTakerSkipReplace = async (gameId: number): Promise<string> => {
+    return (await getData('game' + gameId.toString() + '_taker_skipReplace'))[0].value as string;
+}
+
+export const getBet = async (gameId: number): Promise<number> => {
+    return (await getData('game' + gameId.toString() + '_bet'))[0].value as number;
+}
+
+export const getWaitingExpirationHeight = async (gameId: number): Promise<number> => {
+    return (await getData('game' + gameId.toString() + '_waitingExpirationHeight'))[0].value as number;
+}
+
+export const getExpirationHeight = async (gameId: number): Promise<number> => {
+    return (await getData('game' + gameId.toString() + '_expirationHeight'))[0].value as number;
+}
+
+export const getRange = async (gameId: number, rangePosition: string): Promise<number> => {
+    return (await getData('game' + gameId.toString() + '_range_' + rangePosition))[0].value as number;
+}
+
+export const getMakerRandomsCommit = async (gameId: number): Promise<number> => {
+    return (await getData('game' + gameId.toString() + "_maker_randomsCommit"))[0].value as number;
+}
+
+export const getRandoms = async (gameId: number, playerRole: 'maker' | 'taker'): Promise<number> => {
+    return (await getData('game' + gameId.toString() + "_" + playerRole + "_randoms"))[0].value as number;
+}
+
+export const getStep = async (gameId: number): Promise<number> => {
+    return (await getData('game' + gameId.toString() + '_step'))[0].value as number;
+}
+
+export const getRarity = async (gameId: number, playerRole: 'maker' | 'taker', rangePosition: 'worst' | 'medium' | 'best'): Promise<number> => {
+    return (await getData('game' + gameId.toString() + "_" + playerRole + "_" + rangePosition + "Rarity"))[0].value as number;
+}
+
+export const getTakerOrderCommit = async (gameId: number): Promise<string> => {
+    return (await getData('game' + gameId.toString() + "_taker_orderCommit"))[0].value as string;
+}
+
+export const getOrder = async (gameId: number, playerRole: 'maker' | 'taker'): Promise<string> => {
+    return (await getData('game' + gameId.toString() + "_" + playerRole + "_order"))[0].value as string;
+}
+
+export const getPlayerResult = async (gameId: number, playerAddress: string): Promise<string> => {
+    return (await getData('game' + gameId.toString() + "_" + playerAddress + "_result"))[0].value as string;
+}
+
+// --- STATS ---
+
+export const getPlayerPrize = async (gameId: number, playerAddress: string): Promise<number> => {
+    return (await getData('game' + gameId.toString() + "_" + playerAddress + "_prize"))[0].value as number;
+}
+
+export const getPlayerWins = async (playerAddress: string): Promise<number> => {
+    try {
+        return (await getData(playerAddress + '_wins'))[0].value as number;
+    } catch (err) {
+        return 0
+    }
+}
+
+export const getPlayerLoses = async (playerAddress: string): Promise<number> => {
+    try {
+        return (await getData(playerAddress + '_loses'))[0].value as number;
+    } catch (err) {
+        return 0
+    }
+}
+
+export const getPlayerDraws = async (playerAddress: string): Promise<number> => {
+    try {
+        return (await getData(playerAddress + '_draws'))[0].value as number;
+    } catch (err) {
+        return 0
+    }
+}
+
+export const getPlayerPnL = async (playerAddress: string): Promise<number> => {
+    try {
+        return (await getData(playerAddress + '_PnL'))[0].value as number;
+    } catch (err) {
+        return 0
+    }
+}
+
+export const getGamesPlayed = async (): Promise<number> => {
+    try {
+        return (await getData("gamesPlayed"))[0].value as number;
+    } catch (err) {
+        return 0
+    }
+}
---
+++ src/sdk/V2/gameTransactions.ts
@@ -0,0 +1,133 @@
+import { IInvokeScriptParams } from "@waves/waves-transactions";
+import { EGG_ID, GAME_ADDRESS, CHAIN_ID } from "../../settings";
+
+export const makeGameTx = (
+    slot: number,
+    rarityRangeWorst: number,
+    rarityRangeMedium: number,
+    rarityRangeBest: number,
+    randomsCommit: string,
+    eggs: number,
+): IInvokeScriptParams => ({
+    dApp: GAME_ADDRESS,
+    fee: 500000,
+    call: {
+        function: "makeGame",
+        args: [
+            { type: 'integer', value: slot },
+            { type: 'integer', value: rarityRangeWorst },
+            { type: 'integer', value: rarityRangeMedium },
+            { type: 'integer', value: rarityRangeBest },
+            { type: 'string', value: randomsCommit },
+        ],
+    },
+    payment: [{
+        assetId: EGG_ID,
+        amount: eggs,
+    }],
+    chainId: CHAIN_ID,
+});
+
+export const kickGameTx = (slot: number): IInvokeScriptParams => ({
+    dApp: GAME_ADDRESS,
+    fee: 500000,
+    call: {
+        function: "kickGame",
+        args: [
+            { type: 'integer', value: slot },
+        ],
+    },
+    chainId: CHAIN_ID,
+});
+
+export const takeGameTx = (slot: number, randoms: string, skipReplace: boolean, eggs: number): IInvokeScriptParams => ({
+    dApp: GAME_ADDRESS,
+    fee: 500000,
+    call: {
+        function: "takeGame",
+        args: [
+            { type: 'integer', value: slot },
+            { type: 'string', value: randoms },
+            { type: 'boolean', value: skipReplace },
+        ],
+    },
+    payment: [{
+        assetId: EGG_ID,
+        amount: eggs,
+    }],
+    chainId: CHAIN_ID,
+});
+
+export const revealRandomsAndReplaceOneDuckMakerTx = (makerRandoms: string, salt: string, rangeToReplace: string, duckId: string): IInvokeScriptParams => ({
+    dApp: GAME_ADDRESS,
+    fee: 500000,
+    call: {
+        function: "revealRandomsAndReplaceOneDuckMaker",
+        args: [
+            { type: "string", value: makerRandoms },
+            { type: "string", value: salt },
+            { type: "string", value: rangeToReplace },
+            { type: "string", value: duckId },
+        ]
+    },
+    chainId: CHAIN_ID,
+});
+
+export const replaceOneDuckTakerTx = (rangeToReplace: String, duckId: String): IInvokeScriptParams => ({
+    dApp: GAME_ADDRESS,
+    fee: 500000,
+    call: {
+        function: "replaceOneDuckTaker",
+        args: [
+            { type: "string", value: rangeToReplace },
+            { type: "string", value: duckId },
+        ]
+    },
+    chainId: CHAIN_ID,
+});
+
+export const commitOrderTakerTx = (orderCommit: string): IInvokeScriptParams => ({
+    dApp: GAME_ADDRESS,
+    fee: 500000,
+    call: {
+        function: "commitOrderTaker",
+        args: [
+            { type: "string", value: orderCommit },
+        ]
+    },
+    chainId: CHAIN_ID,
+});
+
+export const setOrderMakerTx = (order: string): IInvokeScriptParams => ({
+    dApp: GAME_ADDRESS,
+    fee: 500000,
+    call: {
+        function: "setOrderMaker",
+        args: [
+            { type: "string", value: order },
+        ]
+    },
+    chainId: CHAIN_ID,
+});
+
+export const revealOrderTakerTx = (order: string, salt: string): IInvokeScriptParams => ({
+    dApp: GAME_ADDRESS,
+    fee: 500000,
+    call: {
+        function: "revealOrderTaker",
+        args: [
+            { type: "string", value: order },
+            { type: "string", value: salt },
+        ]
+    },
+    chainId: CHAIN_ID,
+});
+
+export const getPrizeExpiredTx = (): IInvokeScriptParams => ({
+    dApp: GAME_ADDRESS,
+    fee: 500000,
+    call: {
+        function: "getPrizeExpired",
+    },
+    chainId: CHAIN_ID,
+});

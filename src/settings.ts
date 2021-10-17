19221346141c139405e77f2672220f5f99aa14f6
Makeev Ivan <makeev@phystech.edu>
2021-10-17 20:58:01 +0300

    feat: V2 constants and new EGG

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,28 @@
+// --- COMMON ---
 export const MAKER_SEED = ""
 export const TAKER_SEED = ""
 export const IMPOSTOR_SEED = ""
 
+export const EGG_ID = "AjXHBGsVDeXETQPk3qH4CPbZdfXPyNo5XoBx7jVbtwWG"
+export const WRONG_ASSET_ID = "3VUCuh9dWqe54SdzzNUkXhFXpc8UQynnJVh42B7MjNzN"
+export const WAITING = 8
+export const STEP_DURATION = 5
+
+export const CHAIN_ID = "T"
+
+export const WAVES_NODE = "https://nodes-testnet.wavesnodes.com"
+
+export const MAKER_SALT = "dff8b213b4fb"
+export const TAKER_SALT = "3441badf6795"
+
+export const INCUBATOR_ADDRESS = "3MtBigTsaeevrcJAEwVcr4quwkqcrtnjoAh"
+export const FARMING_ADDRESS = "3NBznG19rEYUAcnukTRTtwJHBVKQLbobVdC"
+
+export const GAME_ADDRESS = "3N6oaapvYkEryHUr4vAiPRXM3iS6kMYXito"
+export const RARITY_PROVIDER_ADDRESS = '3NCman4dCzk1HWWU4DPTr5rsbhvuUCwwUZh';
+
+
+// --- V1 ---
 export const MAKER_WORST_DUCK = "2b85F6Lm7CuReUdR7XEhMYLJ8YLujiZXXSvsFRoD8X7Y" // FFFFFFFF 13
 export const MAKER_MEDIUM_DUCK = "79ii5gyBYuSgW9Agqu7jgU9XBcbJTq63DZL3NXK5Bywt" // EEEEEEEE 22
 export const MAKER_MEDIUM_DUCK2 = "PugXTSxYU9KirtkBM3L3ANrbN8TRHgd1KhugRupDNHD" // AAAAAAAA 27
@@ -18,19 +39,11 @@ export const IMPOSTOR_MEDIUM_DUCK = "DkaR26dUbbi6B38oS93MmpiNmjTDCYXiJZooY2oV1HV
 export const IMPOSTOR_MEDIUM_DUCK2 = "D2dyUPWGZDvB9v8ip1vLfHJqjk73mBWKUhh5odxjv5b4" // EEEEEEEE 22
 export const IMPOSTOR_BEST_DUCK = "4TprF5GgPtNtLPMTe1qjuPYtwd6EVYkCqpeQsqkYFp9L" // CCCCCCCC 37
 
-export const MAKER_SALT = "dff8b213b4fb"
-export const TAKER_SALT = "3441badf6795"
+// --- V2 ---
+export const MAKER_REPLACE_DUCK = "PugXTSxYU9KirtkBM3L3ANrbN8TRHgd1KhugRupDNHD" // AAAAAAAA 27
+export const TAKER_REPLACE_DUCK = "7TckPpGLjcHJ53XVXJ4kXC7aS9vZRhAMSgCtrDGD3K2C" // CCCCCCCC 37 (FARMING)
+export const IMPOSTOR_REPLACE_DUCK = "4TprF5GgPtNtLPMTe1qjuPYtwd6EVYkCqpeQsqkYFp9L" // CCCCCCCC 37
 
-export const INCUBATOR_ADDRESS = "3MtBigTsaeevrcJAEwVcr4quwkqcrtnjoAh"
-export const FARMING_ADDRESS = "3NBznG19rEYUAcnukTRTtwJHBVKQLbobVdC"
+export const MAKER_RANDOMS = "1234,44,-97657,-2,23,-286482784265"
+export const TAKER_RANDOMS = "-736487,235325323,-213,2424,0,11"
 
-export const GAME_ADDRESS = "3N6oaapvYkEryHUr4vAiPRXM3iS6kMYXito"
-export const RARITY_PROVIDER_ADDRESS = '3NCman4dCzk1HWWU4DPTr5rsbhvuUCwwUZh';
-
-export const EGG_ID = "3VUCuh9dWqe54SdzzNUkXhFXpc8UQynnJVh42B7MjNzN"
-export const WAITING = 8
-export const STEP_DURATION = 5
-
-export const CHAIN_ID = "T"
-
-export const WAVES_NODE = "https://nodes-testnet.wavesnodes.com"
